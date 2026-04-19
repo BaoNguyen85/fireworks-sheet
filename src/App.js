a345a64d49324d5deed0ff6b4dbf5ef57f80051b
Nguyen Chi Bao <90488377+BaoNguyen85@users.noreply.github.com>
2026-04-19 15:54:48 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import confetti from "canvas-confetti";
+import "./App.css";
 
 function App() {
+  useEffect(() => {
+    confetti({
+      particleCount: 200,
+      spread: 120,
+      origin: { y: 0.6 },
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>🎉 Welcome 🎉</h1>
+
+      <iframe
+        src="https://docs.google.com/spreadsheets/d/e/1J5E7nezOhJ1N3Xjh1PBjnyjStRyW8-KdwQJtUKRhRGc/pubhtml"
+        width="100%"
+        height="600"
+        title="Google Sheet"
+      />
     </div>
   );
 }
