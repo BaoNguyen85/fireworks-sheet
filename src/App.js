e9b5c7ad3836c80d1566c3ff805b4661bd936301
Nguyen Chi Bao <90488377+BaoNguyen85@users.noreply.github.com>
2026-04-19 16:12:10 +0700

    fix google sheet link

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
       <iframe
         src="https://docs.google.com/spreadsheets/d/1J5E7nezOhJ1N3Xjh1PBjnyjStRyW8-KdwQJtUKRhRGc/edit?gid=2032942343#gid=2032942343"
         width="100%"
-        height="600"
+        height="100%"
         title="Google Sheet"
       />
     </div>
